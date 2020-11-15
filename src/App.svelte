f5904f429185fe9c2cae84465def0659a9375af6
Colin Fendrick <colin.fendrick@gmail.com>
2020-11-15 04:50:03 -0800

    Reactive vars

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,10 @@
   import Importing from './import.svelte';
   import HelloWorld from './hello-world.svelte';
   import Styled from './styling.svelte';
+  import Counter from './counter.svelte';
 </script>
 
 <Importing />
 <HelloWorld />
 <Styled />
+<Counter />
---
+++ src/counter.svelte
@@ -0,0 +1,20 @@
+<script>
+	let count = 0;
+	$: doubled = count * 2; // compute a value from another value
+
+	$: if (count >= 10) { // and can use this to run functions
+		alert(`count is dangerously high!`);
+		count = 9;
+	}
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<button on:click={handleClick}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+	<br />
+
+	doubled is {doubled}
+</button>
